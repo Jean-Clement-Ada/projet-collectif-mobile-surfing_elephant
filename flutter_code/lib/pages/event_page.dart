b01e8ff65eb254f0f7a878729c442eaea0e67000
CorbiatFlorentin <florentin.corbiat@yahoo.fr>
2024-05-06 14:54:13 +0200

    template finis spot

--- flutter_code/lib/pages/event_page.dart
+++ flutter_code/lib/pages/event_page.dart
@@ -1,36 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class EventPage extends StatelessWidget {
+class EventPage extends StatefulWidget {
   const EventPage({super.key});
 
 
-   final events = [
-     {
-       "spotPhoto" : "lien photo",
-       "nameSpot" : "nom du spot",
-       "place" : "lieu",
-       "difficulty": "level",
-     }
-   ];
+
+
+  @override
+  State<EventPage> createState() => _EventPageState();
+}
+class _EventPageState extends State<EventPage> {
+
+  final events = [
+    {
+      "spotPhoto": "lien photo",
+      "nameSpot": "Bisacrosse",
+      "place": "Sud_Ouest",
+      "difficulty": "level",
+    }
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Liste des spots"),
       ),
-      body:  Center(
-        child: ListView(
-          children: [
-            Card(
+      body: Center(
+        child: ListView.builder(
+          itemCount: events.length,
+          itemBuilder: (context, index) {
+            final event = events[index];
+            final spotPhoto = event["spotPhoto"];
+            final nameSpot = event["nameSpot"];
+            final place = event["place"];
+            final difficulty = event["difficulty"];
+
+
+            return Card(
               child: ListTile(
-                leading: Image.asset("image Api"),
-                title: Text("Nom du spot"),
-                subtitle: Text("Lieu" + "difficulté"),
-                trailing: Icon(Icons.abc_sharp),
+                leading: Image.asset("assets/images/logo_app.webp"),
+                title: Text("$nameSpot"),
+                subtitle: Text("$place" + " " + "$difficulty"),
+                trailing: Icon(Icons.more_vert),
               ),
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
