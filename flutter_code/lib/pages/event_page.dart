96d6c9c3af29fe06b4f0a68019d0fc6d1bc8be35
CorbiatFlorentin <florentin.corbiat@yahoo.fr>
2024-05-06 12:34:03 +0200

    listing +template API

--- flutter_code/lib/pages/event_page.dart
+++ flutter_code/lib/pages/event_page.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class EventPage extends StatelessWidget {
   const EventPage({super.key});
 
+
+   final events = [
+     {
+       "spotPhoto" : "lien photo",
+       "nameSpot" : "nom du spot",
+       "place" : "lieu",
+       "difficulty": "level",
+     }
+   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Liste des spots"),
       ),
-      body: const Center(
-        child: Text("prochainement disponible"),
+      body:  Center(
+        child: ListView(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Image.asset("image Api"),
+                title: Text("Nom du spot"),
+                subtitle: Text("Lieu" + "difficulté"),
+                trailing: Icon(Icons.abc_sharp),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
