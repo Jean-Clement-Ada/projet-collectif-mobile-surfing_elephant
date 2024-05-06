afc0fea3b6aeb9ca53930e470041e8460bc3ff08
CorbiatFlorentin <florentin.corbiat@yahoo.fr>
2024-05-06 23:56:43 +0200

    finish BottomBar + redirection to EventPage

--- flutter_code/lib/main.dart
+++ flutter_code/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_code/pages/event_page.dart';
 import 'package:flutter_code/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
@@ -5,29 +6,54 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  int _currentIndex = 0; // sert à définir la page current à 0
+
+  setCurrentIndex(int index) {
+    setState(() {
+      _currentIndex = index; //sert à interroger sur quel page on est pour le modifier ensuite
+
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Surfing Elephant"),
         ),
-        body: HomePage(),
-        bottomNavigationBar:BottomNavigationBar(
+        body: [
+          HomePage(),
+         EventPage()  //avec ca on définis Event comme page 2
+      ][_currentIndex], // et on questionne sur quel page on est avec current
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) => setCurrentIndex(index), //actionne currentindex(donc changement de page depuis la barre).
+          selectedItemColor: Colors.green,
+          unselectedItemColor: Colors.black,
+          iconSize: 32,
+          elevation: 10,
+          // trait d'ombre sur la barre
           items: [
             BottomNavigationBarItem(
                 icon: Icon(Icons.home),
-                 label: "Accueil"
+                label: "Accueil"
             ),
             BottomNavigationBarItem(
                 icon: Icon(Icons.waves),
                 label: "Liste des spots"
             ),
           ],
-        ) ,
+        ),
       ),
     );
   }
