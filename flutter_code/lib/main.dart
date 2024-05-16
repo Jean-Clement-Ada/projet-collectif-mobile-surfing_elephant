ce32e1953d90f7dcd220a7cb9479a0ef2acc6a98
CorbiatFlorentin <florentin.corbiat@yahoo.fr>
2024-05-16 11:54:43 +0200

    Firebase + Init BDD

--- flutter_code/lib/main.dart
+++ flutter_code/lib/main.dart
@@ -3,9 +3,18 @@ import 'package:flutter_code/pages/form_page.dart';
 import 'package:flutter_code/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_code/pages/description_spot_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
